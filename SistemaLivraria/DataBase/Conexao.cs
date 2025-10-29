dbcfad8e6cb1e4740d9b8a769700768ca2755946
CJ3027422 <CJ3027422@LAB02-PC08>
2025-10-29 14:45:57 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaLivraria/DataBase/Conexao.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SistemaLivraria.Database
+{
+    public class Conexao
+    {
+        // String de conexão - AUTENTICAÇÃO SQL SERVER
+        private static string stringConexao =
+            "Data Source=sqlexpress;" +
+            "Initial Catalog=CJ3027422PR2;" +
+            "User ID=aluno;" +
+            "Password=aluno;";
+
+        // Método para obter a conexão
+        public static SqlConnection ObterConexao()
+        {
+            SqlConnection conexao = new SqlConnection(stringConexao);
+            try
+            {
+                conexao.Open();
+                return conexao;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao conectar com o banco de dados: " + ex.Message,
+                                "Erro de Conexão",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        // Método para testar a conexão
+        public static bool TestarConexao()
+        {
+            try
+            {
+                using (SqlConnection conexao = ObterConexao())
+                {
+                    if (conexao != null)
+                    {
+                        conexao.Close();
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SistemaLivraria/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using SistemaLivraria.Database;  // ← Importante!
+
+namespace SistemaLivraria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestarConexao_Click(object sender, EventArgs e)
+        {
+            if (Conexao.TestarConexao())
+            {
+                MessageBox.Show("Conexão realizada com sucesso! ✅",
+                                "Sucesso",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Falha ao conectar com o banco de dados! ❌",
+                                "Erro",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+    }
+}
